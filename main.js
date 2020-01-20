1722abb71ec3a8d718fb102f11d763be23767271
HoJin9622 <kiss0104040@gmail.com>
2020-01-20 15:17:46 +0900

    라우터 - 파일로 분리

--- main.js
+++ main.js
@@ -1,11 +1,9 @@
 const fs = require("fs");
-const path = require("path");
-const qs = require("querystring");
 const express = require("express");
 const bodyParser = require("body-parser");
 const compression = require("compression");
-const sanitizeHtml = require("sanitize-html");
 const template = require("./lib/template");
+const topicRouter = require("./routes/topic");
 
 const app = express();
 const port = 3000;
@@ -19,6 +17,8 @@ app.get("*", (request, response, next) => {
     next();
   });
 });
+
+app.use("/topic", topicRouter);
 
 app.get("/", (request, response) => {
   const title = "Welcome";
@@ -34,112 +34,6 @@ app.get("/", (request, response) => {
     `<a href="/topic/create">create</a>`
   );
   response.send(html);
-});
-
-app.get("/topic/create", (request, response) => {
-  const title = "WEB - create";
-  const list = template.list(request.list);
-  const html = template.HTML(
-    title,
-    list,
-    `
-      <form action="/topic/create_process" method="post">
-        <p><input type="text" name="title" placeholder="title"></p>
-        <p>
-          <textarea name="description" placeholder="description"></textarea>
-        </p>
-        <p>
-          <input type="submit">
-        </p>
-      </form>
-    `,
-    ""
-  );
-  response.send(html);
-});
-
-app.post("/topic/delete_process", (request, response) => {
-  const post = request.body;
-  const id = post.id;
-  const filteredId = path.parse(id).base;
-  fs.unlink(`data/${filteredId}`, error => {
-    response.redirect("/");
-  });
-});
-
-app.get("/topic/update/:pageId", (request, response) => {
-  const filteredId = path.parse(request.params.pageId).base;
-  fs.readFile(`data/${filteredId}`, "utf8", (err, description) => {
-    const title = request.params.pageId;
-    const list = template.list(request.list);
-    const html = template.HTML(
-      title,
-      list,
-      `
-        <form action="/topic/update_process" method="post">
-          <input type="hidden" name="id" value="${title}">
-          <p><input type="text" name="title" placeholder="title" value="${title}"></p>
-          <p>
-            <textarea name="description" placeholder="description">${description}</textarea>
-          </p>
-          <p>
-            <input type="submit">
-          </p>
-        </form>
-        `,
-      `<a href="/topic/create">create</a> <a href="/update?id=${title}">update</a>`
-    );
-    response.send(html);
-  });
-});
-
-app.post("/topic/update_process", (request, response) => {
-  const post = request.body;
-  const id = post.id;
-  const title = post.title;
-  const description = post.description;
-  fs.rename(`data/${id}`, `data/${title}`, error => {
-    fs.writeFile(`data/${title}`, description, "utf8", err => {
-      response.redirect(`/topic/${title}`);
-    });
-  });
-});
-
-app.post("/topic/create_process", (request, response) => {
-  const post = request.body;
-  const title = post.title;
-  const description = post.description;
-  fs.writeFile(`data/${title}`, description, "utf8", err => {
-    response.redirect(`/topic/${title}`);
-  });
-});
-
-app.get("/topic/:pageId", (request, response, next) => {
-  const filteredId = path.parse(request.params.pageId).base;
-  fs.readFile(`data/${filteredId}`, "utf8", (err, description) => {
-    if (err) {
-      next(err);
-    } else {
-      const title = request.params.pageId;
-      const sanitizedTitle = sanitizeHtml(title);
-      const sanitizedDescription = sanitizeHtml(description, {
-        allowedTags: ["h1"]
-      });
-      const list = template.list(request.list);
-      const html = template.HTML(
-        sanitizedTitle,
-        list,
-        `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
-        ` <a href="/topic/create">create</a>
-            <a href="/topic/update/${sanitizedTitle}">update</a>
-            <form action="/topic/delete_process" method="post">
-              <input type="hidden" name="id" value="${sanitizedTitle}">
-              <input type="submit" value="delete">
-            </form>`
-      );
-      response.send(html);
-    }
-  });
 });
 
 app.use((req, res, next) => {
