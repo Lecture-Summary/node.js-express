57bacdd8880898890e9256aca890d9d5e3db28b1
HoJin9622 <kiss0104040@gmail.com>
2020-01-27 02:04:48 +0900

    인증구현 - UI 만들기

--- main.js
+++ main.js
@@ -2,11 +2,12 @@ const fs = require("fs");
 const express = require("express");
 const bodyParser = require("body-parser");
 const compression = require("compression");
-const indexRouter = require("./routes/index");
-const topicRouter = require("./routes/topic");
 const helmet = require("helmet");
 const app = express();
 const port = 3000;
+const indexRouter = require("./routes/index");
+const topicRouter = require("./routes/topic");
+const authRouter = require("./routes/auth");
 app.use(helmet());
 
 app.use(express.static("public"));
@@ -21,6 +22,7 @@ app.get("*", (request, response, next) => {
 
 app.use("/", indexRouter);
 app.use("/topic", topicRouter);
+app.use("/auth", authRouter);
 
 app.use((req, res, next) => {
   res.status(404).send("Sorry cant find that!");
