aef8979ccf4baba67c7b3566699ab1f8af403a91
HoJin9622 <kiss0104040@gmail.com>
2020-01-28 22:13:52 +0900

    인증구현 - 인증상태를 UI에 반영

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,23 @@ const express = require("express");
 const router = express.Router();
 const template = require("../lib/template");
 
+function authIsOwner(request, response) {
+  if (request.session.is_logined) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function authStatusUI(request, response) {
+  let authStatusUI = "<a href='/auth/login'>login</a>";
+  if (authIsOwner(request, response)) {
+    authStatusUI = `${request.session.nickname} | <a href="/auth/logout">logout</a>`;
+  }
+
+  return authStatusUI;
+}
+
 router.get("/", (request, response) => {
   const title = "Welcome";
   const description = "Hello, Node.js";
@@ -13,7 +30,8 @@ router.get("/", (request, response) => {
       <h2>${title}</h2>${description}
       <img src="/images/hello.jpg" style="width:300px; display:block; margin-top:10px;">
       `,
-    `<a href="/topic/create">create</a>`
+    `<a href="/topic/create">create</a>`,
+    authStatusUI(request, response)
   );
   response.send(html);
 });
