093efb191f6f61ffa49291b17553b37a5c61f5e2
HoJin9622 <kiss0104040@gmail.com>
2020-02-03 21:29:35 +0900

    Passport 세션이용 3

--- main.js
+++ main.js
@@ -46,15 +46,11 @@ app.use(passport.session());
 passport.serializeUser(function(user, done) {
   console.log("serializeUser", user);
   done(null, user.email);
-  //done(null, user.id);
 });
 
 passport.deserializeUser(function(id, done) {
   console.log("deserializeUser", id);
   done(null, authData);
-  //User.findById(id, function(err, user) {
-  //done(err, user);
-  //});
 });
 
 passport.use(
